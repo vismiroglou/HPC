a00038feeffc9732e1764797f3b4dca6102f31a5
vism <vismiroglou@gmail.com>
2024-02-22 14:41:04 +0100

    added comments

--- Lecture3/mppi.py
+++ Lecture3/mppi.py
@@ -1,3 +1,17 @@
+'''
+Monte Carlo approximation of pi using multiprocessing.
+Random points in (0,1) are generated and pi is calculated from the formula P = pi/4
+where P is the ratio of points inside the unitary circle vs. the ones outside.
+
+Multiprocessing tests happen in two manners:
+In measure, the processing time is measured as a function of parallelized processes with a maximum
+equal to the available cpu cores.
+
+In measure N, the processing time is measured as a function of realizations, with the mp processes
+set to exactly the maximum available cpu cores. The number of realizations start from 1 and go higher
+than the available processes.
+'''
+
 import multiprocessing as mp
 import random
 import numpy as np
@@ -25,17 +39,9 @@ def parpi(num_workers, L, N):
     points_in_circle = np.sum([result.get() for result in results])
     # Calculate pi approximation as P = pi/4 where P is the ratio of points in the circle over points outside.
     mcpi = 4 * points_in_circle / (N*L)
-    return(mcpi)
+    return(mcpi) 
 
-def pltres(workers, exTimes, familyname):
-    plt.plot(workers, exTimes)
-    plt.xticks(workers)
-    plt.xlabel('Number of workers')
-    plt.ylabel('Time (s)')
-    plt.savefig(f'{familyname}_speedup.png')
-    plt.show()
-
-def measure(L, N, familyname):
+def measure(L, N, familyname = 'num_workers'):
     '''
     Measures the speed of the parallel processing as a function of the number of available processes.
     The available processes start for 1 (no parallel processing) and go up to maximum available cores.
@@ -45,9 +51,17 @@ def measure(L, N, familyname):
         start = time.time()
         parpi(num_workers, L, N)
         times.append(time.time()-start)
-    pltres(range(1, mp.cpu_count()+1), times, familyname)
 
-def measureN(k, familyname, Nmax):
+    #Plot
+    plt.plot(range(1, mp.cpu_count()+1), times)
+    plt.xticks(range(1, mp.cpu_count()+1))
+    plt.xlabel('Number of workers')
+    plt.ylabel('Time (s)')
+    plt.title('Processing time as a function of parallel processes with the maximum being equal to the number of cores')
+    plt.savefig(f'plots/{familyname}_speedup.pdf')
+    plt.show()
+
+def measureN(k, Nmax, familyname='num_realizations'):
     '''
     With the number of workers equal to the available pc cores:
     Measures the processing time as a function of realizations while maintaining the total number
@@ -67,12 +81,13 @@ def measureN(k, familyname, Nmax):
     plt.title('Processing time as a function of N while keeping the total number of points static (k=N*L). Number of cores: 6')
     plt.xticks(range(1, Nmax +1))
     plt.xlim(1, Nmax+1)
+    plt.savefig(f'plots/{familyname}_fixedk.pdf')
     plt.show()
-    plt.savefig(f'{familyname}_fixedk.png')
     plt.close()
     
 
-    
-
 if __name__ == '__main__':
-    measure(10**6, 18, 'testfamily')
+    L = 10**6
+    N = 18
+    measure(L, N)
+    measureN(L*N, 18)
