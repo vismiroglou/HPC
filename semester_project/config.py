964ddc142b106215efe26acf3be6cf1f4b3fcf25
vism <vismiroglou@gmail.com>
2024-02-29 15:12:12 +0100

    Initial commit

---
+++ semester_project/config.py
@@ -0,0 +1,11 @@
+from argparse import Namespace
+config = Namespace(name = None,
+                   pre = 1000,
+                   pim = 1000,
+                   re_floor = -2,
+                   re_ceiling = 1,
+                   im_floor = -1.5,
+                   im_ceiling = 1.5,
+                   num_workers = 6,
+                   I = 100
+                    )
