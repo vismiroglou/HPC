963a8ac252325cf8ae6adb19a69c75f1c342f34e
vism <vismiroglou@gmail.com>
2024-02-29 15:30:51 +0100

    added parallelized experiments

--- semester_project/parallelized_experiments.py
+++ semester_project/parallelized_experiments.py
@@ -5,32 +5,62 @@ to compare performance. The naive loop-version is used as it gets the most out o
 
 import numpy as np
 import multiprocessing as mp
-from naive_mandelbrot import make_grid, mandelbrot, parallelize_grid
+from mandelbrot_loop import make_grid, mandelbrot_numba, plot_mandelbrot
+from mandelbrot_vect import make_grid as make_grid_vect, mandelbrot_vector
+import time
 
+def parallelize_grid(config, num_workers, model):
+    start = time.time()
+    pool = mp.Pool(num_workers)
+    if model == 'loop':
+        re, im = make_grid(config.re_floor, 
+                           config.re_ceiling, 
+                           config.im_floor, 
+                           config.im_ceiling, 
+                           config.pre, 
+                           config.pim)
+        reals = np.array_split(re, num_workers)
+        results = [pool.apply_async(mandelbrot_numba, (real, im, config.I, )) for real in reals]
+        pool.close()
+        pool.join()
+        results = [result.get()[0] for result in results]
+        img = np.concatenate(results, axis=1)
+    elif model == 'vect':
+        c = make_grid_vect(config.re_floor,
+                      config.re_ceiling,
+                      config.im_floor,
+                      config.im_ceiling,
+                      config.pre,
+                      config.pim)
+        subgrids = np.array_split(c, num_workers)
+        results = [pool.apply_async(mandelbrot_vector, (subgrid, config.I, )) for subgrid in subgrids]
+        pool.close()
+        pool.join()
+        results = [result.get()[0] for result in results]
+        img = np.concatenate(results)
+    else:
+        raise ValueError('Invalid model type.')
+    proc_time = time.time() - start
+    return img, proc_time
+
+def parallelize_grid_numba(re, im, I, num_workers):
+    start = time.time()
+    pool = mp.Pool(num_workers)
+    reals = np.array_split(re, num_workers)
+    results = [pool.apply_async(mandelbrot_numba, (real, im, I, )) for real in reals]
+    pool.close()
+    pool.join()
+    results = [result.get()[0] for result in results]
+    img = np.concatenate(results, axis=1)
+    proc_time = time.time()-start
+    return img, proc_time
 
 if __name__ == '__main__':
-    from argparse import ArgumentParser
-    ap = ArgumentParser(prog= 'Loop Mandelbrot',
-                        description= 'Nested loop implementation for the mandelbrot recursive task. Includes JIT and multiprocessing')
-    ap.add_argument('--name', type=str, default='loop_mandelbrot', help='Experiment name')
-    ap.add_argument('--pre', type=int, default=5000, help='Amount of real numbers in the grid')
-    ap.add_argument('--pim', type=int, default=5000, help='Amount of imaginary numbers in the grid')
-    ap.add_argument('--re_floor', type=float, default=-2, help='Lower bound of real component')
-    ap.add_argument('--re_ceiling', type=float, default=1, help='Higher bound of real component')
-    ap.add_argument('--im_floor', type=float, default=-1.5, help='Lower bound of imaginary component')
-    ap.add_argument('--im_ceiling', type=float, default=1.5, help='Higher bound of imaginary component')
-    ap.add_argument('--num_iter', type=int, default=100, help='Number of iterations')
-    args = ap.parse_args()
+    from config import config
+    config.name = 'parallelize_experiments'
 
-    pre = args.pre
-    pim = args.pim
-    re_floor = args.re_floor
-    re_ceiling = args.re_ceiling
-    im_floor = args.im_floor
-    im_ceiling = args.im_ceiling
-    I = args.num_iter
+    num_workers = 6
+    img, proc_time = parallelize_grid(config, num_workers, 'loop')
+    plot_mandelbrot(img, config)
 
-    re, im = make_grid(re_floor, re_ceiling, im_floor, im_ceiling, pre, pim)
-
-    for num_workers in range(12):
-        parallelize_grid(re, im, I, num_workers)
+    
