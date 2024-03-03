40708dac47578af61d8ab3a9b0be58a96a68541a
vismiroglou <vismiroglou@gmail.com>
2024-03-03 14:13:21 +0100

    fixed vect + numba

--- semester_project/mandelbrot_vect.py
+++ semester_project/mandelbrot_vect.py
@@ -1,7 +1,7 @@
 import numpy as np
 import multiprocessing as mp
 from numba import jit, objmode
-from mandelbrot_loop import plot_mandelbrot, plot_time_results
+from mandelbrot_loop import plot_mandelbrot
 import time
 
 def make_grid(re_floor, re_ceiling, im_floor, im_ceiling, pre, pim):
@@ -22,19 +22,23 @@ def mandelbrot_vector(c, I):
     proc_time = time.time() - start
     return img, proc_time
 
-# @jit Does not work yet.
-# def mandelbrot_vector_numba(c, I):
-#     with objmode(start='f8'):
-#         start = time.time()
-#     z = np.zeros_like(c)
-#     img = np.zeros((c.shape[0], c.shape[1]))
-#     for i in range(I):
-#         mask = np.abs(z) <= 2
-#         z[mask] = z[mask] * z[mask] + c[mask]
-#         img += mask.astype(int)
-#     with objmode(proc_time = 'f8'):
-#         proc_time = time.time() - start
-#     return img, proc_time
+@jit
+def mandelbrot_vector_numba(c, I):
+    with objmode(start='f8'):
+        start = time.time()
+    z = np.zeros_like(c)
+    orig_shape = c.shape
+    c = c.flatten()
+    z = z.flatten()
+    img = np.zeros(c.shape)
+    for i in range(I):
+        mask = np.abs(z) <= 2
+        z[mask] = z[mask] * z[mask] + c[mask]
+        img += mask.astype(np.int_)
+    with objmode(proc_time = 'f8'):
+        proc_time = time.time() - start
+    img = img.reshape(orig_shape)
+    return img, proc_time
 
 def parallel_grid(num_workers, c, I):
     start = time.time()
@@ -53,9 +57,14 @@ if __name__ == '__main__':
     config.name = 'vect_mandelbrot'
 
     c = make_grid(config.re_floor, config.re_ceiling, config.im_floor, config.im_ceiling, config.pre, config.pim)
-    img, proc_time =  mandelbrot_vector(c, config.I)
 
-    print(f'Processing time for {config.name}: {proc_time}[s]')
+    models = {"Vect": mandelbrot_vector(c, config.I),
+              "Vect + JIT": mandelbrot_vector_numba(c, config.I)}
+    
+    for model in models:
+        img, proc_time = models[model]
+        print(f'Processing time for {model}: {proc_time}[s]')
+
     plot_mandelbrot(img, config)
 
   
