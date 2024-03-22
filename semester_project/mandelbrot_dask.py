646b56623c51ac9ef0365b287d6e161928913271
vism <vismiroglou@gmail.com>
2024-03-22 08:54:26 +0100

    Defined mandelbrot instead of importing

--- semester_project/mandelbrot_dask.py
+++ semester_project/mandelbrot_dask.py
@@ -1,36 +1,73 @@
-from mandelbrot_vect import mandelbrot_vector as mandelbrot
-from mandelbrot_loop import plot_mandelbrot
+from mandelbrot_vect import make_grid
 import dask.array as da
 from dask.distributed import Client, wait
 import numpy as np
+import time
+from matplotlib import pyplot as plt
 
-# import time
+def mandelbrot(c, I):
+    z = np.zeros_like(c)
+    img = np.zeros((c.shape[0], c.shape[1]))
+    for i in range(I):
+        mask = np.abs(z) <= 2
+        z[mask] = z[mask] * z[mask] + c[mask]
+        img += mask.astype(int)
+    return img
 
-def make_grid(re_floor, re_ceiling, im_floor, im_ceiling, pre, pim):
-    re = da.from_array(np.linspace(re_floor, re_ceiling, pre))
-    im = da.from_array(np.linspace(im_floor, im_ceiling, pim))
-    X, Y = da.from_array(np.meshgrid(re, im))
-    c = X + Y * 1j
-    return c
+if __name__ == '__main__':
+    from config import config
+    
+    # Dask automatically parallelizes processes when creating a Client.
+    # Changing between local and Strato just depends on setting up the Client properly.
+    num_workers = 6
+    I = 100
 
-def parallelize_grid(config, chunks):
+    # client = Client(n_workers=num_workers)
+    client = Client('10.92.1.192:8786')
+
+    # Creating the grid as a numpy array
     c = make_grid(config.re_floor, 
                         config.re_ceiling, 
                         config.im_floor, 
                         config.im_ceiling, 
                         config.pre, 
                         config.pim)
-    img = c.rechunk(chunks=chunks).map_blocks(lambda x: mandelbrot(x, 100))
-    client.close()
-    return(img)
-
-if __name__ == '__main__':
-    from config import config
     
-    num_workers = 6
-    client = Client(n_workers=num_workers)
-    chunks = (100,100)
-    img = parallelize_grid(config, num_workers, chunks)
-    img.compute()
-    plot_mandelbrot(img, config)
+    points = [[],[]]
+
+    # Simple numpy implementation
+    print(f'Starting numpy version')
+    start = time.time()
+    img = mandelbrot(c, I)
+    proc_time = time.time() - start
+    print(f'Processing time{proc_time}')
+    points[0].append('numpy')
+    points[1].append(proc_time)
+
+    # Chunk size comparison 1-D
+    print(f'Starting dask distributed version for multiple chunk sizes:')
+    
+    c = da.from_array(c)
+    for x in [2, 3, 6, 9, 12, 15, 18]:
+        chunks = (config.pre//x, config.pim)
+        print(f'Chunk size: {chunks}')
+        start = time.time()
+        img = c.rechunk(chunks=chunks).map_blocks(lambda x: mandelbrot(x, I)).compute()
+        wait(img)
+        proc_time = time.time() - start
+        print(f'Processing time{proc_time}')
+        points[0].append(str(chunks))
+        points[1].append(proc_time)
+
+    client.close()
+    fig = plt.figure(figsize=(7,5))
+    plt.xticks(rotation=30, fontsize=8, ha='right')
+    plt.title('1-D Chunk size comparison.')
+    plt.plot(points[0], points[1])
+    plt.savefig('handin_2/1d_chunk_size_comparison.png')
+    plt.show()
+
+    
+
+    
     
